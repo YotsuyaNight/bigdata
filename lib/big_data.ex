fdb79975bd3d5752dd27312fe0383a09b6754d24
Marcin Dłubakowski <marcin@dlubakowski.pl>
2023-08-02 15:24:20 +0200

    Init

---
+++ lib/big_data.ex
@@ -0,0 +1,18 @@
+defmodule BigData do
+  @moduledoc """
+  Documentation for `BigData`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> BigData.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/big_data_test.exs
@@ -0,0 +1,8 @@
+defmodule BigDataTest do
+  use ExUnit.Case
+  doctest BigData
+
+  test "greets the world" do
+    assert BigData.hello() == :world
+  end
+end
